92d88a667562e0710ba4c73bc3637a1a12873d7d
Fernando Cunha <cunhafernandofj@gmail.com>
2023-05-22 14:36:07 -0300

    configurando deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:cunhafernando/attendanceofemployees.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :deploy_to, "/home/deploy/attendanceofemployees"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
